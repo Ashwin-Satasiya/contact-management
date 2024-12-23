c40b9b36f751b91d9bd18dd717eeeddd6d63ea9c
Ashwin-Satasiya <ashvinsatasiya@gmail.com>
2024-12-23 19:30:19 +0530

    Data added & updated

--- src/pages/AllContacts.jsx
+++ src/pages/AllContacts.jsx
@@ -1,26 +1,9 @@
 import React from "react";
 
+import { useSelector } from "react-redux";
+
 const AllContacts = () => {
-  const dummyContacts = [
-    {
-      id: 1,
-      name: "John Doe",
-      email: "john@example.com",
-      phone: "123-456-7890",
-    },
-    {
-      id: 2,
-      name: "Jane Smith",
-      email: "jane@example.com",
-      phone: "987-654-3210",
-    },
-    {
-      id: 3,
-      name: "Sam Wilson",
-      email: "sam@example.com",
-      phone: "555-123-4567",
-    },
-  ];
+  const contact = useSelector((state) => state.contact.value);
 
   return (
     <div className="container mt-4" style={{ minHeight: "80vh" }}>
@@ -29,26 +12,29 @@ const AllContacts = () => {
         <table className="table table-bordered table-hover">
           <thead className="table-light">
             <tr>
-              <th scope="col">#</th>
+              <th scope="col">Sr. No.</th>
               <th scope="col">Name</th>
               <th scope="col">Email</th>
               <th scope="col">Phone</th>
-              <th scope="col">Actions</th>
             </tr>
           </thead>
           <tbody>
-            {dummyContacts.map((contact, index) => (
-              <tr key={contact.id}>
-                <th scope="row">{index + 1}</th>
-                <td>{contact.name}</td>
-                <td>{contact.email}</td>
-                <td>{contact.phone}</td>
-                <td>
-                  <button className="btn btn-warning btn-sm me-2">Edit</button>
-                  <button className="btn btn-danger btn-sm">Delete</button>
+            {contact && contact.length > 0 ? (
+              contact.map((contact) => (
+                <tr key={contact.id}>
+                  <td>{contact.id}</td>
+                  <td>{contact.name}</td>
+                  <td>{contact.email}</td>
+                  <td>{contact.phone}</td>
+                </tr>
+              ))
+            ) : (
+              <tr className="text-center">
+                <td colSpan={4} className="fs-3">
+                  Data not available
                 </td>
               </tr>
-            ))}
+            )}
           </tbody>
         </table>
       </div>
